c42d68425a3925ad2b90ab014aa86465c053aac4
clawplusstacker <38380295+clawplusstacker@users.noreply.github.com>
2020-04-14 22:20:48 -0400

    Finished Version 0.1

--- background.js
+++ background.js
@@ -2,30 +2,46 @@
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
 //Function for when the onClicked is called
-function buttonClicked(tab){
+function buttonClicked(){
 
-  var str, strNoSpaces, strSplit, wordCount, charCount;
 
-  chrome.tabs.executeScript({code:"window.getSelection().toString();"},
+  var str, strNoSpaces, strSplit, wordCount, numbersReturn, charCount;
 
-    function wordCounter(results){
+  //Testing if it is a doc or pdf file
 
-       str = results.toString();
 
-       strNoSpaces = str.replace(/  +/g, ' ');
-
-       strSplit = strNoSpaces.split(" ");
-       wordCount = strSplit.length;
-
-        if(strSplit[0] == ""){
-          wordCount --;
-        }
-
-        charCount = str.replace(/  +/g, '').length;
-
-        var numbersReturn = ("There are " + wordCount + " words and " + charCount + " characters (without spaces)!");
-        alert(numbersReturn);
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+      let url = tabs[0].url;
+      if(url.includes("https://docs.google.com/document")){
+        alert("This Extension Does Not Work in Google Docs");
+      }else if(url.includes(".pdf")){
+        alert("This Extension Does Not Work in PDF Files");s
       }
-    );
+      else{
 
+
+        /*If it is not in Docs or a PDF, Calucate Words*/
+
+        chrome.tabs.executeScript({code:"window.getSelection().toString();"}, results =>{
+
+             str = results.toString();
+
+             strNoSpaces = str.replace(/  +/g, ' ');
+
+             strSplit = strNoSpaces.split(" ");
+             wordCount = strSplit.length;
+
+              if(strSplit[0] == ""){
+                wordCount --;
+              }
+
+              charCount = str.replace(/  +/g, '').length;
+
+              numbersReturn = ("There are " + wordCount + " words and " + charCount + " characters (without spaces)!");
+
+              alert(numbersReturn);
+            }
+          );
+        }
+      });
 }
