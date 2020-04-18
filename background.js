68bd22e9c89ca049bf1640a30bd602ca3a2d4864
clawplusstacker <38380295+clawplusstacker@users.noreply.github.com>
2020-04-17 23:40:23 -0400

    Working on Sending Message

--- background.js
+++ background.js
@@ -1,38 +1,44 @@
-/*//Setting up a listener
-chrome.browserAction.onClicked.addListener(buttonClicked)
+chrome.runtime.onMessage.addListener(function(message){
+    if(message.popupOpen) {
+      buttonClicked();
+    }  
+});
 
-//Function for when the onClicked is called
+
+var str, strNoSpaces, strSplit, wordCount, charCount, msg;
+
 function buttonClicked(tab){
 
-  //Declaring variables 
-  var str, strNoSpaces, strSplit, wordCount, charCount, msg;
-
-  //Telling chrome to execute this script, calls function wordCounter 
-  chrome.tabs.executeScript({code:"window.getSelection().toString();"},
-
-  function wordCounter(results){
-      //Getting the results to a string and putting it into an array, where it is then counted
-      //into the wordCount variable 
-       str = results.toString();
-       strNoSpaces = str.replace(/  +/g, ' ');
-       strSplit = strNoSpaces.split(" ");
-       wordCount = strSplit.length;
-        if(strSplit[0] == ""){
-          wordCount --;
-        }
-
-        //Getting the amount of characters minus spaces
-        charCount = str.replace(/  +/g, '').length;
-
-        //Declaring the message variable 
-        msg = "There are " + wordCount + " words and " + charCount + " characters (without spaces)!";
+    //Declaring variables 
+  
+    //Telling chrome to execute this script, calls function wordCounter 
+    chrome.tabs.executeScript({code:"window.getSelection().toString();"},
+  
+    function wordCounter(results){
+       
+         str = results.toString();
+         strNoSpaces = str.replace(/  +/g, ' ');
+         strSplit = strNoSpaces.split(" ");
+         wordCount = strSplit.length;
+          if(strSplit[0] == ""){
+            wordCount --;
+          }
+  
+          //Getting the amount of characters minus spaces
+          charCount = str.replace(/  +/g, '').length;
+  
+          //Declaring the message variable 
+          msg = "There are " + wordCount + " words and " + charCount + " characters (without spaces)!";
+          console.log(msg);
         
-        //Sending a message to the content script 
-        chrome.tabs.sendMessage(tab.id, msg);
+          //Sending a message to the content script
+          //chrome.tabs.sendMessage(tab.id, msg);
+   
+        }
+      );
+    }
 
-      }
-    );
-    
-}
-*/
+
+
+//chrome.browserAction.onClicked.addListener(buttonClicked)
 
