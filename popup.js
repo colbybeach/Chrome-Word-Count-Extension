dfd398a9bf10ba33286b93be1fef57f444f0fece
clawplusstacker <38380295+clawplusstacker@users.noreply.github.com>
2020-04-18 22:19:53 -0400

    Finishing Javascript

--- popup.js
+++ popup.js
@@ -1,41 +1,70 @@
-chrome.tabs.executeScript({code: "window.getSelection().toString();"}, selection => {
+//Looking for if the button is clicked to count text manually 
+document.addEventListener('DOMContentLoaded', function() {
+  
+  let checkPageButton = document.getElementById('subButton'); //Getting HTML button
+
+  checkPageButton.addEventListener('click', function countWordsFunc() {
+    let userInput = document.getElementById('txtField').value;  //Getting the value of text Field
+    countWords(userInput);                     //Putting the text field value through the countWords function
+  });
+});
+
+
+
+
+//Checking for highlighted text 
+chrome.tabs.executeScript({code: 'window.getSelection().toString();'}, selection => {
 
 
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
+    let url = tabs[0].url;  //Gets users URL
 
-    if(url.includes("https://docs.google.com/document/")){
-      document.getElementById("numbersReturn").innerHTML = "This extension does not work with Google Docs";
+    //Tests to see if the URL is in Google Docs 
+    if(url.includes('https://docs.google.com/document/')){
+      document.getElementById('numbersReturn').innerHTML = 'This extension does not work with Google Docs';
 
-    }else if(url.includes(".pdf")){
+    //Tests to see if the URL a PDF 
+    }else if(url.includes('.pdf')){
 
-      document.getElementById("numbersReturn").innerHTML = "This extension does not work with PDF Files";
+      document.getElementById('numbersReturn').innerHTML = 'This extension does not work with PDF Files';
 
+    //Counts highlighted words 
     }else {
           countWords(selection[0]);
-  }
-});
+    }
+  });
   
 });  
 
-  function countWords(x){
-    let str, strNoSpaces, strSplit, wordCount, charCount, msg;
 
-    str = x;
-    strNoSpaces = str.replace(/  +/g, ' ');
-    strSplit = strNoSpaces.split(" ");
-    wordCount = strSplit.length;
-    if(strSplit[0] == ""){
+
+
+ /**Count words function count amount of words in a string
+  * param x == String that will be counted 
+  * Returns nothing, rather changes the HTML to number 
+  */
+
+  function countWords(x){
+
+    let strNoSpaces, strSplit, wordCount, charCount, msg;
+
+    //Getting the number of words 
+    strNoSpaces = x.replace(/  +/g, ' '); //Replaces all double spaces with single space
+    strSplit = strNoSpaces.split(' ');    //Splits string into an array without spaces
+    wordCount = strSplit.length;          //Counts the array 
+
+    if(strSplit[0] == ''){                //Subtracts one so if no text is inputted the count is still '0'
       wordCount --;
     }
     
     
     //Getting the amount of characters minus spaces
-    charCount = str.replace(/  +/g, '').length;
-    msg = "There are " + wordCount + " words and " + charCount + " characters!"; 
+    charCount = x.replace(/  +/g, '').length;
+    msg = 'There are ' + wordCount + ' words and ' + charCount + ' characters!'; 
 
-    document.getElementById("numbersReturn").innerHTML = msg;
+
+
+    document.getElementById('numbersReturn').innerHTML = msg; //Changes HTML
   }
-
 
 
